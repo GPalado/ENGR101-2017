9ed721300a16ff746beac756cf2c9e2e44a6b066
EricDiputado <ericdiputado@gmail.com>
2016-04-14 16:27:30 +1200

    Eric - Error fix + comments
    changed printf to print the number, and also added comments which says what the code does

--- number-printer.c
+++ number-printer.c
@@ -1,7 +1,11 @@
 #include  <stdio.h>
 
+//main method which prints a number
+//declares a variable
+//prints the variable
 int  main(){
   int  number = 100;
-  printf("%c", number);
+  printf("%d", number);
   return  0;
+  
 }
